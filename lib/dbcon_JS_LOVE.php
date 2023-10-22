aa8f962a3ecb3db2101f6a762a11571e0bbc5731
윤희동 <jini1237@naver.com>
2023-10-23 07:33:46 +0900

    111
    11111

--- lib/dbcon_JS_LOVE.php
+++ lib/dbcon_JS_LOVE.php
@@ -1,11 +1,23 @@
 <?php
-
+error_reporting( E_ALL );
+ini_set( "display_errors", 1 );
 
 $real_hostname="localhost";
 $real_username="root";
 //$real_password="apmsetup";
 $real_password="autoset";
 $real_name="js_love";
+
+
+
+
+$real_hostname="3.36.83.6";
+$real_username="cfgaussroot";
+$real_password="autoset";
+
+
+
+
 $real_sock = mysqli_connect($real_hostname, $real_username, $real_password, $real_name);
 $real_db = mysqli_select_db($real_sock, $real_name) or die ("데이터베이스 서버에 연결할 수 없습니다.");
 mysqli_set_charset($real_sock, 'utf8'); 
