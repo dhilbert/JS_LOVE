ef036b9d2ba1cd7b23e4a1997c8a5dd06b1a37ad
윤희동 <jini1237@naver.com>
2023-10-23 15:19:33 +0900

    1111
    11111

--- contents_header.php
+++ contents_header.php
@@ -28,7 +28,7 @@
 <meta property="og:type" content="website">
 <meta property="og:url" content="http://3.36.83.6/JS_LOVE/">
 <meta property="og:title" content="수호네트웍크!!!!">
-<meta property="og:image" content="https://dispatch.cdnser.be/cms-content/uploads/2019/12/01/7e488e45-364e-4a4b-bb68-fa4a7f430af2.jpg">
+<meta property="og:image" content="http://3.36.83.6/JS_LOVE/KakaoTalk_20230929_203256499.jpg">
 <meta property="og:description" content="수호 네트웍크 프로토타입.">
 
 
